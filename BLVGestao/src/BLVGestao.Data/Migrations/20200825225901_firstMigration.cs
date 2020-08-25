7eb10822432f5f634fa388b9780a4eec6b70a50e
Diogo Cavassani <diogocavassani@gmail.com>
2020-08-25 20:04:14 -0300

    Finalização da camada de Dominio, ORM e Migrations.

---
+++ BLVGestao/src/BLVGestao.Data/Migrations/20200825225901_firstMigration.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BLVGestao.Data.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormasDePagamento",
+                columns: table => new
+                {
+                    FormaDePagamentoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    PrazoRecebimento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormasDePagamento", x => x.FormaDePagamentoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GrupoAcesso",
+                columns: table => new
+                {
+                    GrupoAcessoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Permissao = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GrupoAcesso", x => x.GrupoAcessoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(type: "bool", nullable: false),
+                    TipoPessoa = table.Column<int>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    DataNascimento = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Cpf = table.Column<string>(type: "varchar(11)", maxLength: 11, nullable: true),
+                    Rg = table.Column<string>(type: "varchar(12)", maxLength: 12, nullable: true),
+                    RazaoSocial = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    NomeFantasia = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    Cnpj = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.PessoaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(type: "bool", nullable: false),
+                    GrupoAcessoId = table.Column<int>(nullable: false),
+                    Login = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true),
+                    Senha = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_GrupoAcesso_GrupoAcessoId",
+                        column: x => x.GrupoAcessoId,
+                        principalTable: "GrupoAcesso",
+                        principalColumn: "GrupoAcessoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    EnderecoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    PessoaId = table.Column<int>(nullable: false),
+                    Logradouro = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    Numero = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: true),
+                    Bairro = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.EnderecoId);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "PessoaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(type: "bool", nullable: false),
+                    FornecedorId = table.Column<int>(nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Observacao = table.Column<string>(nullable: true),
+                    Unidade = table.Column<string>(type: "varchar(3)", maxLength: 3, nullable: false),
+                    ValorVenda = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    ValorCusto = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.ProdutoId);
+                    table.ForeignKey(
+                        name: "FK_Produto_Pessoa_FornecedorId",
+                        column: x => x.FornecedorId,
+                        principalTable: "Pessoa",
+                        principalColumn: "PessoaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telefone",
+                columns: table => new
+                {
+                    TelefoneId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    PessoaId = table.Column<int>(nullable: false),
+                    Numero = table.Column<string>(type: "varchar(13)", maxLength: 13, nullable: false),
+                    TipoTelefone = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefone", x => x.TelefoneId);
+                    table.ForeignKey(
+                        name: "FK_Telefone_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "PessoaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    VendaId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClienteId = table.Column<int>(nullable: false),
+                    UsuarioId = table.Column<int>(nullable: false),
+                    FormaDePagamentoId = table.Column<int>(nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Situacao = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.VendaId);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Pessoa_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Pessoa",
+                        principalColumn: "PessoaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vendas_FormasDePagamento_FormaDePagamentoId",
+                        column: x => x.FormaDePagamentoId,
+                        principalTable: "FormasDePagamento",
+                        principalColumn: "FormaDePagamentoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estoque",
+                columns: table => new
+                {
+                    EstoqueId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    ProdutoId = table.Column<int>(nullable: false),
+                    Quantidade = table.Column<decimal>(type: "decimal", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estoque", x => x.EstoqueId);
+                    table.ForeignKey(
+                        name: "FK_Estoque_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movimentacoes",
+                columns: table => new
+                {
+                    MovimentacaoId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    ProdutoId = table.Column<int>(nullable: false),
+                    TipoMovimento = table.Column<int>(nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimentacoes", x => x.MovimentacaoId);
+                    table.ForeignKey(
+                        name: "FK_Movimentacoes_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContaReceber",
+                columns: table => new
+                {
+                    ContaReceberId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ativo = table.Column<bool>(nullable: false),
+                    VendaId = table.Column<int>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal", nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContaReceber", x => x.ContaReceberId);
+                    table.ForeignKey(
+                        name: "FK_ContaReceber_Pessoa_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Pessoa",
+                        principalColumn: "PessoaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContaReceber_Vendas_ContaReceberId",
+                        column: x => x.ContaReceberId,
+                        principalTable: "Vendas",
+                        principalColumn: "VendaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensVenda",
+                columns: table => new
+                {
+                    VendaId = table.Column<int>(nullable: false),
+                    ProdutoId = table.Column<int>(nullable: false),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensVenda", x => new { x.VendaId, x.ProdutoId });
+                    table.ForeignKey(
+                        name: "FK_ItensVenda_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItensVenda_Vendas_VendaId",
+                        column: x => x.VendaId,
+                        principalTable: "Vendas",
+                        principalColumn: "VendaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContaReceber_ClienteId",
+                table: "ContaReceber",
+                column: "ClienteId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereco_PessoaId",
+                table: "Endereco",
+                column: "PessoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estoque_ProdutoId",
+                table: "Estoque",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensVenda_ProdutoId",
+                table: "ItensVenda",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimentacoes_ProdutoId",
+                table: "Movimentacoes",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_FornecedorId",
+                table: "Produto",
+                column: "FornecedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telefone_PessoaId",
+                table: "Telefone",
+                column: "PessoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_GrupoAcessoId",
+                table: "Usuarios",
+                column: "GrupoAcessoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_ClienteId",
+                table: "Vendas",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_FormaDePagamentoId",
+                table: "Vendas",
+                column: "FormaDePagamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_UsuarioId",
+                table: "Vendas",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContaReceber");
+
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Estoque");
+
+            migrationBuilder.DropTable(
+                name: "ItensVenda");
+
+            migrationBuilder.DropTable(
+                name: "Movimentacoes");
+
+            migrationBuilder.DropTable(
+                name: "Telefone");
+
+            migrationBuilder.DropTable(
+                name: "Vendas");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "FormasDePagamento");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+
+            migrationBuilder.DropTable(
+                name: "GrupoAcesso");
+        }
+    }
+}
